14cd1ecb892860b17f19ee00a8f22f3be5622afc
AntonKilk <ant.kilk@gmail.com>
2021-07-01 18:28:40 +0300

    d02 ex 06 done

--- d02/ex06/ft_putnbr.c
+++ d02/ex06/ft_putnbr.c
@@ -7,17 +7,39 @@ void ft_putchar(char c)
 }
 
 void ft_putnbr(int nb)
-{
-    char c = nb + '0';
-}
+{    
+    if(nb == 0)
+    {
+        ft_putchar('0');
+    }
+    else 
+    {
+        if(nb < 0)
+        {
+            nb *= -1;
+            ft_putchar('-');
+        }
+        char ch;
+        char arr[30];
+        int i = 0;
+        while (nb > 0)
+        {   
+            ch = nb % 10 + '0';  
+            arr[i] = ch;     
+            nb /= 10; 
+            i++;
+        }
+        while (i > 0)
+        {
+            ft_putchar(arr[i-1]);
+            i--;
+        }
+    }
+}           
 
 int main(void)
 {   
-    printf("Checking function ft_putnbr with positive integer \n");
-    ft_putnbr(42);
-    printf("\nChecking function ft_putnbr with zero \n");
-    ft_putnbr(0);
-    printf("\nChecking function ft_putnbr with negative integer \n");
-    ft_putnbr(-5);
+    printf("Checking function with positive integer: \n");
+    ft_putnbr(-2147483648);
     return 0;
 }
