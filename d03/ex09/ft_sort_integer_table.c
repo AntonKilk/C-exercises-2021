9920e6fc04b389ac6d0cf84fca70597dfad1a7cf
AntonKilk <ant.kilk@gmail.com>
2021-07-11 09:26:28 +0300

    d03 ex09 done

---
+++ d03/ex09/ft_sort_integer_table.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+void ft_swap(int *a, int *b)
+{
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int sort_integer_table(int *tab, int size)
+{
+    int ii, jj, temp; 
+
+    ii = 0;
+    while (ii < size - 1)
+    {   
+        jj = size - 1;
+        while (jj > ii)
+        {   
+            if (tab[jj-1] > tab[jj])
+            {
+                temp = tab[jj-1];
+                tab[jj -1] = tab[jj];
+                tab [jj] = temp;
+            }
+            jj--;            
+        }
+        ii++;
+    }
+    return *tab;
+}
+
+int main(void)
+{
+    int arr[10] = {1, 42, 21, 5, 634 ,2, 21, -1, 25, -10};
+    int i = 0;
+    while (i < 10)
+    {
+        printf("%d \n", arr[i]);
+        i++;
+    }
+    sort_integer_table(arr, 10);
+
+    printf("\nSorted array: \n");
+    i = 0;
+    while (i < 10)
+    {
+        printf("%d \n", arr[i]);
+        i++;
+    }
+}
